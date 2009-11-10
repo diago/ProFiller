078b8714fda4fe399169d8fd73798c21be6ef28f
Heath <heath.padrick@outlandweb.com>
2009-11-10 12:55:26 -0500

    FIX better regex for finding checkbox data

--- form_filler.js
+++ form_filler.js
@@ -72,7 +72,7 @@ var FormFiller = Class.create( {
 		var id = id || this.id;
 		var data = this.data.get(id) || false;
 		if(!data && this.id.endsWith(']')){
-			var exp = new RegExp("\\[.*\\]");
+			var exp = new RegExp("\\[.*\\]$");
 			var real_id = this.id.sub(exp, '');
 			data = this._find(real_id);
 		}
