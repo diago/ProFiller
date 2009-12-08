e96ff4a97275e872a422a74236e2e7eda42394a9
Heath <heath.padrick@outlandweb.com>
2009-12-08 09:20:28 -0500

    VERSION 2.0.0

--- profiller.js
+++ profiller.js
@@ -24,111 +24,63 @@ var ProFiller = (function(){
 	
 	var ProFiller = Class.create();
 	
-	ProFiller.Version = '1.0.0';
+	ProFiller.Version = "2.0.0";
 	
 	ProFiller.options = {
-		onStart : Prototype.emptyFunction,
-		onComplete : Prototype.emptyFunction			
+		reset: true
 	};
 	
 	ProFiller.prototype = {
-		initialize : function(form, options) {
+		initialize: function(form, options){
 			this.options = Object.extend(ProFiller.options, options || {});
-		
 			this.form = $(form);
 			this.elements = this.form.getElements();
-			
 		},
-		/**
-		 * Fills the form with the supplied data
-		 */
-		fill : function(data) {
 		
+		fill: function(data){
+			
+			var value, name, type;
+			
+			if(this.options.reset) this.form.reset();
 			this.data = $H(data);
 			
-			this.options.onStart(this.form);
-
 			this.elements.each(function(elem){
-				this.elem = $(elem);
-				this.id = this.elem.identify();
-				this.name = this.elem.readAttribute('name').sub('[]', '');;
-				this.value = this.elem.readAttribute('value');
-				this.type = '_' + this.elem.tagName.toLowerCase();
-				this[this.type]();
+				name = elem.readAttribute('name').sub('[]', '');
+				value = this.data.get(name);
+				type = elem.readAttribute('type');
+				if(Object.isArray(value)){
+					for(var i=0;i<value.length;i++){
+						this._setValue(elem, type, value[i])
+					}
+				} else this._setValue(elem, type, value);
 			}.bind(this));
-		
-			this.options.onComplete(this.form);
-		
-		},
-		
-		/**
-		 * Reset the form with the initial data
-		 */
-		reset : function() {
-			this.form.reset();
-		},
-		
-		_find: function(id){
-			var id = id || this.id;
-			var data = this.data.get(id) || this.data.get(this.name) || false;
-			if(!data && this.id.include('::')){
-				var exp = new RegExp("\\:\\:.*$");
-				var real_id = this.id.sub(exp, '');
-				data = this._find(real_id);
-			}
-			return data;
-		},
-		
-		_input : function() {
-		
-			switch(this.elem.readAttribute('type').toLowerCase()){
-			case 'checkbox' :
-				var values = this._find();
-				if(values.indexOf(this.value) !== -1){
-					this.elem.checked = true;
-				}
-				break;
-				
-			case 'radio' :
-				this.elem.checked = ( this.value == this._find() );
-				break;
-				
-			case 'text' :
-				this.elem.value = this._find() || '';
-				break;
-			}
-		
-		},
-		
-		_select : function() {
 			
-			var value;
-			var values = [];
-			if(this.elem.multiple){
-				values = this._find();
-			} else {
-				value = this._find();
-			}
-		
-			var options = $A(this.elem.options);	
-			options.each(function(o) {
-		
-				if (o.value == value || $A(values).indexOf(o.value) !== -1) {
-		
-					o.selected = true;
-		
-				}
-		
-			}.bind(this));
-		
+			return this.form;
 		},
 		
-		_textarea : function() {
-		
-			this.elem.value = this._find() || '';
-		
-		}			
+		_setValue: function(elem, type, value){
+			var elemValue = elem.readAttribute('value');
+			switch(type){
+				case 'radio':
+					(elemValue == value) ? elem.setValue(value) : false;
+				break;
+				
+				case 'checkbox':
+					(elemValue == value) ? elem.setValue(value) : false;
+				break;
+				
+				default:
+					elem.setValue(value);
+				break;
+			}
+		}
 	};
+	
+	Element.addMethods('FORM', {
+		fill: function(form, data ,resetBeforeFill){
+			return new ProFiller(form,{reset: resetBeforeFill}).fill(data);
+		}
+	});
 	
 	return ProFiller;
 })();
